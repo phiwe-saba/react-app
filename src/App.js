180caf7777e263ac2bf1b22d601f74f31bf3a96c
phiwe saba <sabasbulele81@gmail.com>
2024-03-11 21:19:27 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    searchMovies("The");
+    searchMovies("Hangover");
   }, []);
 
   const searchMovies = async (title) => {
